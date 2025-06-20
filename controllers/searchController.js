8a7013b3051781f922319732ad55626ea8f9c329
Ananya <justdiscussanything@gmail.com>
2025-06-20 20:36:43 +0530

    priority-search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,8 +4,22 @@ const SERVICES = require('../data/services.js');
 function searchServices(query){
 
     //TODO: use fuse for including fuzzy search
+    query = query.toLowerCase();
+    const results = [];
+    const alternativeResults = [];
+    
+    SERVICES.forEach((service)=>{
 
-    return SERVICES.filter((service)=>service.toLowerCase().includes(query.toLowerCase()))
+    const priority = service.toLowerCase().indexOf(query);
+        
+    if(priority == 0){ // if match starts at the beginning
+            results.push(service);
+        }else if( priority != -1 ){
+            alternativeResults.push(service); // if not then any match
+        }
+    })
+
+   return results.length > 0 ? results.sort() : alternativeResults.sort();
 }
 module.exports = searchServices; 
 
